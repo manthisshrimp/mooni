042186e0fa9ad5b95ebb3a09e6cc1a1993bf1d18
Aldus Botha <aldusbotha@gmail.com>
2020-07-17 23:36:56 +0200

    JS evaluation

--- textareas.js
+++ textareas.js
@@ -1,8 +1,26 @@
 
-$('#input_textarea').on('input selectionchange propertychange', function() {
+$('#input_textarea').on('input selectionchange propertychange', function () {
     processInput($(this).val())
 })
 
 function processInput(input) {
-    $('#output_textarea').val(input)
+    var lines = splitLines(input)
+    var outputLines = []
+    lines.forEach(function (line) {
+        var outputLine = evaluateLine(line)
+        outputLines.push(outputLine)
+    });
+    var output = outputLines.join('\n')
+    $('#output_textarea').val(output)
 }
+
+function evaluateLine(line) {
+    try {
+        line = line.replace(/%/g, '/100');
+        return eval(line)
+    } catch (err) {
+        return ''
+    }
+}
+
+function splitLines(t) { return t.split(/\r\n|\r|\n/); }
